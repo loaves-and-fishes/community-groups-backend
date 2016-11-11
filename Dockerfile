f22f9efe73f0b9bf76d462031ff647b8c0f82a0c
Ilya Radchenko <ilya@burstcreations.com>
2016-11-11 16:54:56 -0500

    Update

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,14 @@ FROM node:6.9
 
 ENV PORT 80
 
+RUN npm install -g denali-js/denali#1a0a74b
 RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
 
 COPY package.json /usr/src/app/
 RUN npm install --production
 
 COPY . /usr/src/app
-WORKDIR /usr/src/app
 
 EXPOSE 80
 
--- app/actions/invite.js
+++ app/actions/invite.js
@@ -1,11 +1,9 @@
 import ApplicationAction from './application';
 
 export default class InviteAction extends ApplicationAction {
-  async respond(params) {
+  respond(params) {
     let email = params.data.attributes.email;
     let mailer = this.service('mailer');
-    debugger;
-    await mailer.send('invite-organizer', { email });
-    debugger;
+    mailer.send('invite-organizer', { email });
   }
 }
